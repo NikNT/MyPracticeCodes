9c8b0994f40734ee0e43cf5652f44c29f5ec8219
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2024-07-02 19:16:18 -0400

    1. VueCrash - CDN | CreateVue - Initial Setup

---
+++ 2.VueCrash/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User : Active</p>
+  <p v-else-if="status === 'pending'">User : Pending</p>
+  <p v-else>User : Inactive</p>
+
+  <h3>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
+  </h3>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "John Doe",
+      status: "active",
+      tasks: ["Task One", "Task Two", "Task Three"],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
