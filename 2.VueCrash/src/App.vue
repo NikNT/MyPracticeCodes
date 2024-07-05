dfae69068cad24df5de42ee4cdf462b4d89c625b
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2024-07-04 21:26:13 -0400

    4. Vue - NavBar, Hero, HomeCards | Intro to Props

--- 2.VueCrash/src/App.vue
+++ 2.VueCrash/src/App.vue
@@ -1,85 +1,11 @@
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status === 'active'">User : Active</p>
-  <p v-else-if="status === 'pending'">User : Pending</p>
-  <p v-else>User : Inactive</p>
-
-  <form @submit.prevent="addTask">
-    <label for="newTask">Add Task</label>
-    <input type="text" id="newTask" name="newTask" v-model="newTask" />
-    <button type="submit">Submit</button>
-  </form>
-
-  <h3>
-    <ul>
-      <li v-for="(task, index) in tasks" :key="task">
-        <span>{{ task }}</span>
-        <button @click="deleteTask(index)">x</button>
-      </li>
-    </ul>
-  </h3>
-  <button @click="toggleStatus">Toggle</button>
+  <Navbar />
+  <Hero title="Test Title" subtitle="Test Subtitle" />
+  <HomeCards />
 </template>
 
-<!-- <script>
-import { ref } from "vue";
-export default {
-  setup() {
-    const name = ref("John Doe");
-    const status = ref("active");
-    const tasks = ref(["Task One", "Task Two", "Task Three"]);
-
-    const toggleStatus = () => {
-      if (status.value === "active") {
-        status.value = "pending";
-      } else if (status.value === "pending") {
-        status.value = "inactive";
-      } else {
-        status.value = "active";
-      }
-    };
-
-    return { name, status, tasks, toggleStatus };
-  },
-};
-</script> -->
-
 <script setup>
-import { ref, onMounted } from "vue";
-const name = ref("John Doe");
-const status = ref("active");
-const tasks = ref(["Task One", "Task Two", "Task Three"]);
-const newTask = ref("");
-
-const toggleStatus = () => {
-  if (status.value === "active") {
-    status.value = "pending";
-  } else if (status.value === "pending") {
-    status.value = "inactive";
-  } else {
-    status.value = "active";
-  }
-};
-
-const addTask = () => {
-  if (newTask.value.trim() !== "") {
-    tasks.value.push(newTask.value);
-    newTask.value = "";
-  }
-};
-
-const deleteTask = (index) => {
-  tasks.value.splice(index, 1);
-};
-
-onMounted(async () => {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const data = await response.json();
-
-    tasks.value = data.map((task) => task.title);
-  } catch (error) {
-    console.error(`Error fetching tasks : ${error}`);
-  }
-});
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
 </script>
