8cbcb32d5ad7c2099a6a2a6ffb91373c754c4c9f
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2024-07-05 15:57:59 -0400

    5.HomePage - Done | Next - Router

--- 2.VueCrash/src/App.vue
+++ 2.VueCrash/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Navbar />
-  <Hero title="Test Title" subtitle="Test Subtitle" />
+  <Hero />
   <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
 </template>
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
 import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
 </script>
