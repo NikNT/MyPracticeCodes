f05f8e4400a1044158ee3e37b5dbecf6a53393bd
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2024-07-13 09:38:51 -0400

    12. Golang - Simple Values and Variables

---
+++ 3.Go/3_variables/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	// string
+	// var name string = "golang"
+	var name = "golang"
+
+	//bool
+	var isActive = true
+
+	//int
+	var age = 8;
+
+	//shothand syntax
+	newName := "golang"
+
+	// values can be defined after
+	var after string;
+	after = "after"
+
+	//float
+	var price float32 = 50.6
+	var price2 = 50.6
+	price3 := 50.6
+
+	fmt.Println(name, isActive, age, newName, after, price, price2, price3)
+}
